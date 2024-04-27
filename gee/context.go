e982789e830931a650557f53a1bb76cab7e85949
Xucreat <Xu_H_Lu@163.com>
2024-04-27 06:59:07 -0400

    gee/context

--- gee/context.go
+++ gee/context.go
@@ -1,6 +1,10 @@
 package gee
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 /* 封装 Request 和 Response，提供JSON、HTML等返回类型的支持 */
 /* 设计Context结构，扩展性和复杂性留在内部，对外简化接口。 */
@@ -26,6 +30,41 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 	}
 }
 
-func (c.Context) PostForm(key string) string {
+func (c Context) PostForm(key string) string {
 	return c.Req.FormValue(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
 }
